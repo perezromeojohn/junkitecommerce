ddee9c5e12abb55fe03d778dee97a1ab49916ac1
Rumyooo <201911621@gordoncollege.edu.ph>
2022-05-23 05:13:31 +0800

    v1.7
    apply code not working

--- resources/views/livewire/cart-component.blade.php
+++ resources/views/livewire/cart-component.blade.php
@@ -58,19 +58,32 @@
             <div class="summary">
                 <div class="order-summary">
                     <h4 class="title-box">Order Summary</h4>
-                    <p class="summary-info"><span class="title">Subtotal</span><b class="index">{{Cart::instance('cart')->subtotal()}}</b></p>
-                    <p class="summary-info"><span class="title">Tax</span><b class="index">{{Cart::instance('cart')->tax()}}</b></p>
-                    <p class="summary-info"><span class="title">Shipping</span><b class="index">Free Shipping</b></p>
-                    <p class="summary-info total-info "><span class="title">Total</span><b class="index">{{Cart::instance('cart')->total()}}</b></p>
+                    <p class="summary-info"><span class="title">Subtotal</span><b class="index">₱{{Cart::instance('cart')->subtotal()}}</b></p>
+                    @if(Session::has('coupon'))
+                        <p class="summary-info"><span class="title">Discount ({{Session::get('coupon')['code']}}) <a href="#" wire:click.prevent="removeCoupon"><i class="fa fa-times text-danger"></i></a></span><b class="index">-₱{{$discount}}</b></p>
+                        <p class="summary-info"><span class="title">Subtotal with Discount</span><b class="index">₱{{$subtotalAfterDiscount}}</b></p>
+                        <p class="summary-info"><span class="title">Tax ({{config('cart.tax')}})</span><b class="index">₱{{$taxAfterDiscount}}</b></p>
+                        <p class="summary-info total-info "><span class="title">Total</span><b class="index">₱{{$totalAfterDiscount}}</b></p>
+                    @else
+                        <p class="summary-info"><span class="title">Tax</span><b class="index">{{Cart::instance('cart')->tax()}}</b></p>
+                        <p class="summary-info"><span class="title">Shipping</span><b class="index">Free Shipping</b></p>
+                        <p class="summary-info total-info "><span class="title">Total</span><b class="index">{{Cart::instance('cart')->total()}}</b></p>
+                    @endif
                 </div>
                 <div class="checkout-info">
+                    @if(!Session::has('coupon'))
                     <label class="checkbox-field">
                         <input class="frm-input " name="have-code" id="have-code" value="1" type="checkbox" wire:model="haveCouponCode"><span>I have coupon code</span>
                     </label>
                     @if(!$haveCouponCode != 1)
                     <div class="summary-item">
-                        <form action="">
+                        <form wire:submit.prevent="applyCouponCode">
                             <h4 class="title-box">Coupon Code</h4>
+                            @if(Session::has('coupon_message'))
+                                <div class="alert alert-danger">
+                                    {{ Session::get('coupon_message') }}
+                            @endif
+
                             <p class="row-in-form">
                                 <label for="coupon-code">Enter Coupon Code:</label>
                                 <input type="text" name="coupon code" wire:model="couponCode">
@@ -79,6 +92,7 @@
                         </form>
                     </div>
                     @endif
+                @endif
                     <a class="btn btn-checkout" href="checkout.html">Check out</a>
                     <a class="link-to-shop" href="shop.html">Continue Shopping<i class="fa fa-arrow-circle-right" aria-hidden="true"></i></a>
                 </div>
